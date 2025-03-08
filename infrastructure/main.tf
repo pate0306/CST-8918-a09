ecb2269d1baf858fdccfa3e1719bd3576df5a39e
root <root@Payarth>
2025-03-07 19:21:40 -0500

    first commit

---
+++ infrastructure/main.tf
@@ -0,0 +1,20 @@
+provider "azurerm" {
+  features {}
+}
+
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myResourceGroup"
+  location = "East US"
+}
+
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "mystoragepate0306"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+ 

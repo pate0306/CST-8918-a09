6dfcfa25dde11cc2ef6fcfad1dfe9258c0876aaa
pate0306 <pate0306@algonquinlive.com>
2025-03-07 19:33:05 -0500

    fixed tflint issue

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -9,6 +9,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 
+
 resource "azurerm_storage_account" "storage" {
   name                     = "mystoragepate0306"
   resource_group_name      = azurerm_resource_group.rg.name
